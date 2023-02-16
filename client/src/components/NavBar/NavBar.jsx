39050900ac1601b601c0cda8ee5c49c266eabba1
Lucas <luqasmagra@gmail.com>
2023-02-16 18:33:02 -0300

    Add navbar component

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  return (
+    <header className={styles.header}>
+      <h1>Tables</h1>
+    </header>
+  );
+}
