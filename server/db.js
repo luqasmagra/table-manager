ec9f19ceb2d29e3b7cd90bdb32e33955c33a5770
Lucas <luqasmagra@gmail.com>
2023-02-16 00:22:03 -0300

    Fix environment variables v3

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const URI = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@tablemanager.ompqtnz.mongodb.net/table-manager`;
+const URI = `mongodb+srv://${process.env.USER}:${process.env.PASS}@tablemanager.ompqtnz.mongodb.net/table-manager`;
 
 export async function connectDb() {
   try {
