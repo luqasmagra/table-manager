3b6d11b61ef2ebf18df28ab59bec1577ea10ec7b
Lucas <luqasmagra@gmail.com>
2023-02-17 13:57:16 -0300

    Add Home Page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import TableList from "../../components/TableList/TableList";
+
+export default function Home() {
+  return <TableList />;
+}
--- client/src/pages/TableDetails.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TableDetails() {
-  return <h1>Detalles de mesa</h1>;
-}
