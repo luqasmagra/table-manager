da6ec7d4aab554491a4469b8d8b9acbb5fc1af9d
Lucas <luqasmagra@gmail.com>
2023-02-17 13:56:33 -0300

    Add create table form component

--- client/src/components/TableForm/TableForm.jsx
+++ client/src/components/TableForm/TableForm.jsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_TABLE, GET_TABLES } from "../../graphql/tables";
+import styles from "./TableForm.module.css";
 
-export default function TableForm() {
-  return <h1>TableForm</h1>;
+export default function TableForm({ visible, onClose }) {
+  const [table, setTable] = useState({ name: "" });
+  const [createTable, { loading, error }] = useMutation(CREATE_TABLE, {
+    refetchQueries: [{ query: GET_TABLES }, "GetTables"],
+  });
+
+  const handleOnClose = (e) => {
+    if (e.target.id === "tableFormModal") onClose();
+  };
+  if (!visible) return null;
+
+  const handleChange = ({ target: { name, value } }) => {
+    setTable({ ...table, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createTable({
+      variables: {
+        name: table.name,
+      },
+    });
+    onClose();
+  };
+
+  return (
+    <div
+      id="tableFormModal"
+      className={styles.mainContainer}
+      onClick={handleOnClose}
+    >
+      {loading ? (
+        <span className="loader"></span>
+      ) : (
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            {error && <p>{error.message}</p>}
+            <input
+              className={styles.name}
+              type="text"
+              name="name"
+              placeholder="e.g Mesa 1"
+              onChange={handleChange}
+            />
+            <button className={styles.create} disabled={!table.name}>
+              +
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
--- client/src/components/TableList/TableList.jsx
+++ client/src/components/TableList/TableList.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
-import TableCard from "../TableCard/TableCard";
 import { GET_TABLES } from "../../graphql/tables";
 
 import styles from "./TableList.module.css";
+import TableForm from "../TableForm/TableForm";
+import TableCard from "../TableCard/TableCard";
 
 export default function TableList() {
   const { loading, error, data } = useQuery(GET_TABLES);
-
-  if (loading) return <span className={styles.loader}></span>;
-  if (error) return <span className={styles.error}>Error</span>;
+  const [open, setOpen] = useState(false);
+  const handleOnClose = () => setOpen(false);
+  const handleOpen = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
 
   return (
-    <>
-      {data?.tables.map(({ _id, name, prize }) => {
-        return <TableCard key={_id} _id={_id} name={name} prize={prize} />;
-      })}
-    </>
+    <main className={styles.mainContainer}>
+      {loading ? (
+        <span className={styles.loader}>
+          <span className="loader"></span>
+        </span>
+      ) : error ? (
+        <span className={styles.error}>
+          <span>Error de servidor</span>
+        </span>
+      ) : (
+        <div className={styles.container}>
+          {data?.tables.map(({ _id, name, prize }) => {
+            return <TableCard key={_id} _id={_id} name={name} prize={prize} />;
+          })}
+          <button className={styles.createTable} onClick={handleOpen}>
+            +
+          </button>
+          <TableForm onClose={handleOnClose} visible={open} />
+        </div>
+      )}
+    </main>
   );
 }
