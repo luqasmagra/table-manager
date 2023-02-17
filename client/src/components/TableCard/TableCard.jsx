bffff77ab846718c7ce3e37ed53549519d134e07
Lucas <luqasmagra@gmail.com>
2023-02-17 13:55:07 -0300

    Add Link to Table Details

--- client/src/components/TableCard/TableCard.jsx
+++ client/src/components/TableCard/TableCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./TableCard.module.css";
 
 export default function TableCard({ _id, name, prize, products }) {
   return (
-    <div className={styles.mainContainer}>
+    <Link to={_id} className={styles.mainContainer}>
       <div className={styles.container}>
         <h1 className={styles.title}>{name}</h1>
         <h2 className={styles.prize}>$ {prize}</h2>
       </div>
-    </div>
+    </Link>
   );
 }
