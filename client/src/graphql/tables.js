608274b15e8b35f1712675523f9186b706c586ed
Lucas <luqasmagra@gmail.com>
2023-02-17 13:56:59 -0300

    Add create table querie

--- client/src/graphql/tables.js
+++ client/src/graphql/tables.js
@@ -15,3 +15,13 @@ export const GET_TABLES = gql`
     }
   }
 `;
+
+export const CREATE_TABLE = gql`
+  mutation ($name: String) {
+    createTable(name: $name) {
+      _id
+      name
+      prize
+    }
+  }
+`;
