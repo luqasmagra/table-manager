0711ac6d7c4f191accac4410c4623c16c0776c07
Lucas <luqasmagra@gmail.com>
2023-02-18 04:27:29 -0300

    Fix navigate link

--- client/src/components/TableCard/TableCard.jsx
+++ client/src/components/TableCard/TableCard.jsx
@@ -5,7 +5,7 @@ import styles from "./TableCard.module.css";
 export default function TableCard({ _id, name, prize }) {
   return (
     <>
-      <Link to={_id} className={styles.mainContainer}>
+      <Link to={`/mesa/${_id}`} className={styles.mainContainer}>
         <div className={styles.container}>
           <h1 className={styles.title}>{name}</h1>
           <h2 className={styles.prize}>$ {prize}</h2>
