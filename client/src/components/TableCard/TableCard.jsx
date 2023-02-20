db0f8cab83cdf800536808a2686999822e8e2d39
Lucas <luqasmagra@gmail.com>
2023-02-20 14:15:58 -0300

    Fix TableCard styles

--- client/src/components/TableCard/TableCard.jsx
+++ client/src/components/TableCard/TableCard.jsx
@@ -5,8 +5,8 @@ import styles from "./TableCard.module.css";
 export default function TableCard({ _id, name, prize }) {
   return (
     <>
-      <Link to={`/mesa/${_id}`} className={styles.mainContainer}>
-        <div className={styles.container}>
+      <Link to={`/mesa/${_id}`} className={styles.container}>
+        <div className={styles.description}>
           <h1 className={styles.title}>{name}</h1>
           <h2 className={styles.prize}>$ {prize}</h2>
         </div>
