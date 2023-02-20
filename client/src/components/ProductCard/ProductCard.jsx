2c08823a4bde98f12e9187a2eb9dff9f69c459fe
Lucas <luqasmagra@gmail.com>
2023-02-20 14:15:38 -0300

    Add ProductCard component

---
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "antd";
+import { DeleteOutlined, Loading3QuartersOutlined } from "@ant-design/icons";
+import { useMutation } from "@apollo/client";
+import { DELETE_PRODUCT } from "../../graphql/products";
+import { GET_TABLE } from "../../graphql/tables";
+import styles from "./ProductCard.module.css";
+
+export default function ProductCard({ id, name, prize, quantity }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT, {
+    refetchQueries: [{ query: GET_TABLE }, "getTable"], // vuelvo a realizar la consulta para que se actualice la TableList
+  });
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    await deleteProduct({ variables: { id } });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h4>{quantity}</h4>
+      </div>
+      <div className={styles.description}>
+        <h4>{name}</h4> - <h4>${prize} c/u</h4>
+      </div>
+      {loading ? (
+        <Loading3QuartersOutlined spin="true" />
+      ) : (
+        <Button
+          type="secondary"
+          className={styles.delete}
+          icon={<DeleteOutlined />}
+          onClick={handleDelete}
+        />
+      )}
+    </div>
+  );
+}
