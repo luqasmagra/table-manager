675d88c0432266b611ea093c95a9fbeacce47788
Lucas <luqasmagra@gmail.com>
2023-02-20 16:26:05 -0300

    Add navigate button

--- client/src/components/TableDetails/TableDetails.jsx
+++ client/src/components/TableDetails/TableDetails.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { Button } from "antd";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import { PlusCircleOutlined, LeftOutlined } from "@ant-design/icons";
 import { GET_TABLE } from "../../graphql/tables";
 import ProductCard from "../ProductCard/ProductCard";
 import styles from "./TableDetails.module.css";
 
 export default function TableDetails() {
+  const navigate = useNavigate();
   const params = useParams();
   const { loading, error, data } = useQuery(GET_TABLE, {
     variables: {
@@ -17,7 +18,7 @@ export default function TableDetails() {
   });
 
   return (
-    <main className="mainContainer">
+    <section className="mainContainer">
       {loading ? (
         <span className="containerLoader">
           <span className="loader"></span>
@@ -29,8 +30,18 @@ export default function TableDetails() {
       ) : (
         <div className={styles.container}>
           <div className={styles.header}>
-            <h1>{data?.table.name}</h1>
             <Button
+              title="Mesas"
+              type="secondary"
+              size={"large"}
+              className={styles.goBack}
+              onClick={() => navigate(-1)}
+            >
+              <LeftOutlined />
+            </Button>
+            <h1 className={styles.title}>{data?.table.name}</h1>
+            <Button
+              title="Agregar producto"
               type="secondary"
               size={"large"}
               className={styles.addProduct}
@@ -39,21 +50,25 @@ export default function TableDetails() {
             </Button>
           </div>
           <div className={styles.productList}>
-            {data?.table.products.map(({ _id, name, prize, quantity }) => {
-              return (
-                <ProductCard
-                  key={_id}
-                  id={_id}
-                  name={name}
-                  prize={prize}
-                  quantity={quantity}
-                />
-              );
-            })}
+            {data?.table.products.length ? (
+              data?.table.products.map(({ _id, name, prize, quantity }) => {
+                return (
+                  <ProductCard
+                    key={_id}
+                    id={_id}
+                    name={name}
+                    prize={prize}
+                    quantity={quantity}
+                  />
+                );
+              })
+            ) : (
+              <p title="Click + para agregar productos">No hay productos</p>
+            )}
           </div>
           <h2>Precio total: ${data?.table.prize}</h2>
         </div>
       )}
-    </main>
+    </section>
   );
 }
