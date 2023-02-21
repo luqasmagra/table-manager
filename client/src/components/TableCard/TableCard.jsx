64182affa92bd5af1596686efb62eccb11355717
Lucas <luqasmagra@gmail.com>
2023-02-21 13:17:20 -0300

    Add title

--- client/src/components/TableCard/TableCard.jsx
+++ client/src/components/TableCard/TableCard.jsx
@@ -5,7 +5,11 @@ import styles from "./TableCard.module.css";
 export default function TableCard({ _id, name, prize }) {
   return (
     <>
-      <Link to={`/mesa/${_id}`} className={styles.container}>
+      <Link
+        to={`/mesa/${_id}`}
+        className={styles.container}
+        title="Click para ver detalles"
+      >
         <div className={styles.description}>
           <h1 className={styles.title}>{name}</h1>
           <h2 className={styles.prize}>$ {prize}</h2>
